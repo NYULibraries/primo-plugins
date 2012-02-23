e82857b3d3170412b33f677a0b3ae3afd791d756
Scot Dalton <scotdalton@gmail.com>
2012-02-23 12:03:33 -0500

    Updating tests and classpath

---
+++ testsrc/edu/nyu/libraries/util/AllUtilTests.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package edu.nyu.libraries.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { DataWarehouseConnectionModuleTest.class, 
+	DataWarehouseModuleTest.class, DataWarehouseTest.class })
+
+/**
+ * @author Scot Dalton
+ *
+ */
+public class AllUtilTests {
+}
--- testsrc/edu/nyu/libraries/util/DataWarehouseConnectionModuleTest.java
+++ testsrc/edu/nyu/libraries/util/DataWarehouseConnectionModuleTest.java
@@ -6,6 +6,7 @@ package edu.nyu.libraries.util;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.annotation.Annotation;
 import java.sql.Driver;
 
 import org.junit.Before;
