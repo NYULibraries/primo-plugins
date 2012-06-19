14dbf5ed4130b018e0ff3a14998edc7d63edd423
Scot Dalton <scotdalton@gmail.com>
2012-06-19 16:17:53 -0400

    Capify

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,15 +45,14 @@ namespace :deploy do
   task :create_symlink do
     puts "Skipping symlink creation."
   end
+  
+  desc <<-DESC
+  DESC
+  task :update_code do
+  end
 
   desc <<-DESC
-    Touches up the released code. This is called by update_code after the basic \
-    deploy finishes. Overrides internal implementation since the internal \
-    implementation assumes rails.
-
-    This task will make the release group-writable (if the :group_writable \
-    variable is set to true, which is the default). It will copy the latest \
-    release to the custom directory and cleanup the releases.
+    
   DESC
   task :finalize_update, :except => { :no_release => true } do
   end
